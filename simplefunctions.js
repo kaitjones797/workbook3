c86fa7ba6f29d54bee39661d2994d840c8f750ec
Kaitlyn Jones <kaitlynjones797@gmail.com>
2023-10-23 14:56:00 -0400

    created a function for my fav things

---
+++ simplefunctions.js
@@ -0,0 +1,19 @@
+const myName = "Kaitlyn"
+let favoriteMovie = "Stepbrothers"
+let favoriteBand = "Fleetwood Mac"
+
+
+function favoriteThings() {
+ console.log(myName);   
+ console.log(favoriteMovie);
+ console.log(favoriteBand);
+
+}
+favoriteThings()
+
+let reason = "stability"
+let 
+
+function whyImHere() {
+    
+}
